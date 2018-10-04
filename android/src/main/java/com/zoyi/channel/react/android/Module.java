2362e21609400419e780e8a1c865a661e9cb955b
Mika Jihoon Yoon <mika@zoyi.co>
2018-10-04 15:37:13 +0900

    Add method

--- android/src/main/java/com/zoyi/channel/react/android/Module.java
+++ android/src/main/java/com/zoyi/channel/react/android/Module.java
@@ -1,6 +1,8 @@
 package com.zoyi.channel.react.android;
 
 import com.facebook.react.bridge.*;
+import com.zoyi.channel.plugin.android.*;
+import com.facebook.react.bridge.ReadableMap;
 
 public class Module extends ReactContextBaseJavaModule {
 
@@ -18,5 +20,39 @@ public class Module extends ReactContextBaseJavaModule {
   @ReactMethod
   public void getDeviceId(Callback callback) {
     callback.invoke(Utils.getWId());
+  }
+
+  @ReactMethod
+  public void boot(ReadableMap pluginSettingsJs) {
+    String pluginKey = pluginSettingsJs.getString("pluginKey");
+    String userId = pluginSettingsJs.getString("userId");
+    String locale = pluginSettingsJs.getString("locale");
+    Boolean debugMode = pluginSettingsJs.getString("debugMode");
+    Boolean enabledTrackDefaultEvent = pluginSettingsJs.getString("enabledTrackDefaultEvent");
+    Boolean hideDefaultInAppPush = pluginSettingsJs.getString("hideDefaultInAppPush");
+
+    ChannelPluginSettings channelPluginSettings = ChannelPluginSettings.create(pluginKey)
+        .setUserId(userId)
+        .setLocale(Locale.fromString(locale))
+        .setDebugMode(debugMode)
+        .setEnabledTrackDefaultEvent(enabledTrackDefaultEvent)
+        .setHideDefaultInAppPush(hideDefaultInAppPush);
+
+    ChannelIO.boot(channelPluginSettings);
+  }
+
+  @ReactMethod
+  public void show() {
+    ChannelIO.show();
+  }
+
+  @ReactMethod
+  public void hide() {
+    ChannelIO.show();
+  }
+
+  @ReactMethod
+  public void shutdown() {
+    ChannelIO.shutdown();
   }
 }
